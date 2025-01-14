28776891cc35342576d89b7f7bf495e0a1792ee0
Lina <usuariomac@MacBook-Pro-de-Usuario.local>
2025-01-14 16:28:17 -0500

    cambios en el archivo config

--- src/model/db/config/database.js
+++ src/model/db/config/database.js
@@ -1,10 +1,26 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "12345",
+    "password": "root12345",
     "database": "reservaBatara",
     "host": "127.0.0.1",
-    "port": 3312,
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "root12345",
+    "database": "reservaBatara",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "root12345",
+    "database": "reservaBatara",
+    "host": "127.0.0.1",
+    "port": 3306,
     "dialect": "mysql"
   }
 }
